ac4a2c60df341215e04ef537f72cdd60a99a2d20
capulinGOAT <jmezzomo@alumno.huergo.edu.ar>
2025-08-27 10:11:53 -0300

    Update dayone_game.py

--- dayone_game.py
+++ dayone_game.py
@@ -10,15 +10,17 @@ def main(estado, screen):
     global estado_juego
     estado_juego = estado
     done = False
+    back_to_menu = False
     screen.fill(BLACK)
     pygame.display.flip()
     clock = pygame.time.Clock()
 
     while not done:
-        
+        for event in pygame.event.get():
+            pass
 
         clock.tick(60)
         pygame.display.flip()
 
     
-    return estado_juego
+    return estado_juego, back_to_menu
