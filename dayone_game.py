2ea6d9430e6e49ed3c2ca1a09372b58f7db10687
capulinGOAT <jmezzomo@alumno.huergo.edu.ar>
2025-08-27 09:21:33 -0300

    Update dayone_game.py

--- dayone_game.py
+++ dayone_game.py
@@ -1,11 +1,24 @@
+import pygame, random, os, datetime
 
+BLACK = (0, 0, 0)
+
+pygame.init()
 
 estado_juego = {}
 
-def main(estado):
+def main(estado, screen):
     global estado_juego
     estado_juego = estado
+    done = False
+    screen.fill(BLACK)
+    pygame.display.flip()
+    clock = pygame.time.Clock()
 
+    while not done:
+        
+
+        clock.tick(60)
+        pygame.display.flip()
 
     
     return estado_juego
