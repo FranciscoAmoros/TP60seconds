785687ab6e1485a04f34a6e8a1dd847df71fa741
LRojic <famorosleveratto@alumno.huergo.edu.ar>
2025-09-10 10:22:35 -0300

    Update game.py

--- game.py
+++ game.py
@@ -8,13 +8,13 @@ def start_game(estado, screen):
     global estado_juego, etapa_recoleccion
     estado_juego = estado
 
+    print("inicado juego")
+
     if not estado_juego["dia"] == 0:
         etapa_recoleccion = False
 
     if etapa_recoleccion:
-        estado_juego, back_to_menu = dayone_game.main(estado_juego, screen)
-        if not back_to_menu:
-            estado_juego = seconds_game.main(estado_juego, screen)
+        estado_juego = dayone_game.main(estado_juego, screen)
         estado_juego["dia"] = 1
     else:
         estado_juego = seconds_game.main(estado_juego, screen)
