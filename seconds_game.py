c658348c4cc88c024bfbea45c782a5a8e21131b7
lonik2 <lonyszczyk@alumno.huergo.edu.ar>
2025-10-20 10:18:03 -0300

    eventos
    agregue los posibles eventos

--- seconds_game.py
+++ seconds_game.py
@@ -1,10 +1,21 @@
-
+import pygame, random
 
 estado_juego = {}
 
 def main(estado, screen):
     global estado_juego
     estado_juego = estado
+    
+    done = False
+
+    while not done:
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                done = True
+
+            screen.fill((100, 100, 100))
+            pygame.display.flip()
 
 
     return estado_juego
